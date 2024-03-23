476b1442daba991f7ac38daf1ad19288b3959a7e
vinayakmhavarkar <vinayakmhavarkar@gmail.com>
2024-03-23 23:57:28 +0530

    with jeson value

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+# Define your JSON data
+data = {
+    "1": "one",
+    "2": "two"
+}
+
 @app.route('/')
 def index():
     return 'Welcome to the home page!'
 
+@app.route('/<key>')
+def get_value(key):
+    if key in data:
+        return jsonify(data[key])
+    else:
+        return jsonify({"error": "Key not found"}), 404
+
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
